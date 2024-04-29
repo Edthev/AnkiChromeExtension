9e3b0a85c8d0b6c7ef98c48a18a18ed3fec726c3
Edward Vargas <edthevargas@gmail.com>
2024-04-28 21:52:57 -0400

    added buttons for each top level deck

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,9 @@
 import invoke from "./ankiConnectInvoke.js"; //action, version, params
 import ankiDeckObject from "./deckObject.js";
 
-console.log(Object.keys(ankiDeckObject));
+const listOfDecks = document.getElementById("deckList");
+Object.keys(ankiDeckObject).forEach((key) => {
+   const button = document.createElement("button");
+   button.textContent = key;
+   listOfDecks.appendChild(button);
+});
