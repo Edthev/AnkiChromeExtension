595277d0c37c5a47e79af25dadc5879019a76562
Edward Vargas <edthevargas@gmail.com>
2024-04-29 10:14:27 -0400

    added button handler

--- scripts/script.js
+++ scripts/script.js
@@ -2,8 +2,26 @@ import invoke from "./ankiConnectInvoke.js"; //action, version, params
 import ankiDeckObject from "./deckObject.js";
 
 const listOfDecks = document.getElementById("deckList");
+const handleButtonClick = (key) => {
+   listOfDecks.innerHTML = "";
+   if (key == []) {
+      listOfDecks.innerHTML = "empty";
+   }
+   console.log(Object.keys(ankiDeckObject[key]));
+   Object.keys(ankiDeckObject[key]).forEach((key) => {
+      const button = document.createElement("button");
+      button.textContent = key;
+      listOfDecks.appendChild(button);
+      button.addEventListener("click", () => {
+         handleButtonClick(key);
+      });
+   });
+};
 Object.keys(ankiDeckObject).forEach((key) => {
    const button = document.createElement("button");
    button.textContent = key;
    listOfDecks.appendChild(button);
+   button.addEventListener("click", () => {
+      handleButtonClick(key);
+   });
 });
