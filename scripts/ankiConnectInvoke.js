fcd3004a466e3778eaffa421927f4583f0ee6524
Edward Vargas <edthevargas@gmail.com>
2024-05-03 22:48:34 -0400

    handle anki isn't open error

--- scripts/ankiConnectInvoke.js
+++ scripts/ankiConnectInvoke.js
@@ -1,10 +1,12 @@
 // TODO update port
 let PORT = 8080 || 8765;
 // TODO update address
+// TODO Failed to load resource: net::ERR_CONNECTION_REFUSED 0.0.0.0:8080/:1
+
 let ADDRESS = "0.0.0.0" || "127.0.0.1" || "localhost";
 
 const invoke = (action, version = 6, params = {}) => {
-   return new Promise((resolve, reject) => {
+   return new Promise(async (resolve, reject) => {
       const xhr = new XMLHttpRequest();
       xhr.addEventListener("error", () => reject("failed to issue request"));
       xhr.addEventListener("load", () => {
@@ -29,6 +31,13 @@ const invoke = (action, version = 6, params = {}) => {
       });
 
       xhr.open("POST", `http://${ADDRESS}:${PORT}`);
+      xhr.onerror = function () {
+         const deckList = document.getElementById("deckList");
+         const divElement = document.createElement("div");
+         divElement.innerHTML = "You forgot to open anki";
+         deckList.appendChild(divElement);
+         console.error("Edward");
+      };
       xhr.send(JSON.stringify({ action, version, params }));
    });
 };
