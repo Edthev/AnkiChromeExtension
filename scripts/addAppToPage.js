f4e6d2bea9ff360c073c30eaa20b14caf3d62d51
Edward Vargas <edthevargas@gmail.com>
2024-05-06 15:49:12 -0400

    added placeholder elements to test

--- scripts/addAppToPage.js
+++ scripts/addAppToPage.js
@@ -1,2 +1,9 @@
-const addAppToPage = () => {};
+const addAppToPage = () => {
+   const divElement = document.createElement("div");
+   divElement.innerText = "Testing Edward's extension";
+   divElement.setAttribute("id", "AnkiChromeExtensionParentElement");
+   readingTimeElement.classList.add("", "");
+   const body = document.body;
+   body.insertBefore(divElement, body.firstChild);
+};
 export default addAppToPage;
